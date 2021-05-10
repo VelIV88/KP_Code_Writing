1e7426bfe9ad2e415c7c16c5fb80b0dcd35587b2
Xopt <pia88@yandex.ru>
2021-05-10 15:05:41 +0600

    Edit comments

--- facade_cranc_rod.py
+++ facade_cranc_rod.py
@@ -273,7 +273,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         xLenA_B = -a_B * np.cos(CSM.angleGuid)
         yLenA_B = -a_B * np.sin(CSM.angleGuid)
         
-        #Костыль, угол не всегда рассчитывается правильно.
+        #Костыль.
+        #Иногда угол определяется с ошибкой в 180 градусов.
         #В чем проблема не совсем понятно.
         isAngleAccRodTrue = (xPointA_B - (xPointA_A + xLenA_BA * np.cos(CSM.angleGuid)) < 0.001)
         if not isAngleAccRodTrue:
